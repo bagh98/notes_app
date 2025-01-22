b5af45ab7a3f0fd865f7ba0b297c458b68fec5fe
bagh98~ <abonaorz@gmail.com>
2025-01-22 22:08:16 +0300

    creat note model

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -2,16 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:nots_app/views/widgets/custom_app_bar.dart';
 import 'package:nots_app/views/widgets/custom_text_field.dart';
 
-import 'custom_buttom.dart';
-
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: const SingleChildScrollView(
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(height: 60),
